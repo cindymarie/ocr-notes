46f2ca2f203bd739de276e259bb76e95ed927152
Kevin Jude <kevin@kevin-desktop.(none)>
2011-06-21 15:57:31 +0800

    Initial commit

---
+++ src/keendy/projects/CameraActivity.java
@@ -0,0 +1,7 @@
+package keendy.projects;
+
+import android.app.Activity;
+
+public class CameraActivity extends Activity {
+
+}
---
+++ src/keendy/projects/HomeActivity.java
@@ -0,0 +1,36 @@
+package keendy.projects;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class HomeActivity extends Activity implements OnClickListener {
+  
+  /** Called when the activity is first created. */
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+    
+    /** Initialize Buttons */
+    View snapButton = findViewById(R.id.button1);
+    snapButton.setOnClickListener(this);
+    View searchButton = findViewById(R.id.button2);
+    searchButton.setOnClickListener(this);
+    View aboutButton = findViewById(R.id.button3);
+    aboutButton.setOnClickListener(this);
+  }
+
+  @Override
+  public void onClick(View v) {
+	switch(v.getId()) {
+	  case R.id.button1:
+		break;
+	  case R.id.button2:
+		break;
+	  case R.id.button3:
+		break;
+	}
+  }
+}
