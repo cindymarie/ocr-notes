2bf494c5a72b33e8d86332f60bc41c1e4f870dd8
Kevin Jude Deloria <akiwarheit@gmail.com>
2011-06-22 20:33:15 +0800

    Added camera preview functionality, Fixed camera permission being inside the application tag

--- gen/keendy/projects/R.java
+++ gen/keendy/projects/R.java
@@ -15,12 +15,10 @@ public final class R {
     }
     public static final class id {
         public static final int about_content=0x7f050000;
-        public static final int button1=0x7f050004;
-        public static final int button2=0x7f050005;
-        public static final int button3=0x7f050006;
-        public static final int button4=0x7f050003;
-        public static final int layout=0x7f050001;
-        public static final int preview=0x7f050002;
+        public static final int button1=0x7f050002;
+        public static final int button2=0x7f050003;
+        public static final int button3=0x7f050004;
+        public static final int camera_surface=0x7f050001;
     }
     public static final class layout {
         public static final int about=0x7f030000;
