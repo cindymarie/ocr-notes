643ed376e8b578a02c96cbb2b7c75f2da8b6e18e
Kevin Jude Deloria <akiwarheit@gmail.com>
2011-06-28 14:49:27 +0800

    Change-Id: I0000000000000000000000000000000000000000 Added temporary buttons and changed layout

--- src/keendy/projects/CameraActivity.java
+++ src/keendy/projects/CameraActivity.java
@@ -1,8 +1,8 @@
 package keendy.projects;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.OutputStream;
+
 import android.app.Activity;
 import android.content.ContentValues;
 import android.graphics.PixelFormat;
@@ -14,66 +14,64 @@ import android.util.Log;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
-import android.view.SurfaceHolder.Callback;
 import android.view.View.OnClickListener;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
-public class CameraActivity extends Activity implements Callback { 
+public class CameraActivity extends Activity implements SurfaceHolder.Callback {
 
   private static final String TAG = "CAMERA ACTIVITY";
-  
+
   private SurfaceView mSurfaceView;
   private SurfaceHolder mSurfaceHolder;
-  
+
   private ImageButton mImageButton;
-  
+
   private LinearLayout mLinearLayout;
-  
+
   private Camera mCamera;
-  
+
   private boolean mPreviewRunning = false;
-  
+
   @Override
   public void onCreate(Bundle savedInstanceState) {
 	super.onCreate(savedInstanceState);
-	
-    getWindow().setFormat(PixelFormat.TRANSLUCENT);    
-    
-    mLinearLayout = (LinearLayout) findViewById(R.id.camera_linear_layout);
-    
-    mSurfaceView = (SurfaceView) findViewById(R.id.camera_surface);
-    mSurfaceHolder = mSurfaceView.getHolder();
-    mSurfaceHolder.addCallback(this);
-    mSurfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
-   
-    mImageButton = (ImageButton) findViewById(R.id.camera_button);
-    
-    mImageButton.setOnClickListener(new OnClickListener() {
 
+	getWindow().setFormat(PixelFormat.TRANSLUCENT);
+
+	mLinearLayout = 
+	    (LinearLayout) findViewById(R.id.camera_linear_layout_button);
+
+	mSurfaceView = (SurfaceView) findViewById(R.id.camera_surface);
+	mSurfaceHolder = mSurfaceView.getHolder();
+	mSurfaceHolder.addCallback(this);
+	mSurfaceHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+	mImageButton = (ImageButton) findViewById(R.id.camera_button);
+
+	mImageButton.setOnClickListener(new OnClickListener() {
 	  @Override
 	  public void onClick(View view) {
 		Log.i(TAG, "Class Name: " + view.getClass().getName());
 		Log.i(TAG, "View ID: " + view.getId());
-		if(view.getId() == mImageButton.getId())
+		if (view.getId() == mImageButton.getId())
 		  mCamera.takePicture(null, null, jpegCallback);
 	  }
-    
-    });
-    
-    setContentView(R.layout.camera);
-    
-    Log.i(TAG, "onCreated!");
+	});
+
+	setContentView(R.layout.camera);
+
+	Log.i(TAG, "onCreated!");
   }
 
   @Override
   public void surfaceChanged(SurfaceHolder holder, int format, int w, int h) {
-	
-	if(mPreviewRunning)	{
+
+	if (mPreviewRunning) {
 	  mCamera.stopPreview();
 	}
-	
+
 	Camera.Parameters p = mCamera.getParameters();
 	p.setPreviewSize(w, h);
 	mCamera.setParameters(p);
@@ -92,8 +90,8 @@ public class CameraActivity extends Activity implements Callback {
   @Override
   public void surfaceCreated(SurfaceHolder holder) {
 	mCamera = Camera.open();
-	
-    Log.i(TAG, "surfaceCreated!");
+
+	Log.i(TAG, "surfaceCreated!");
   }
 
   @Override
@@ -102,38 +100,35 @@ public class CameraActivity extends Activity implements Callback {
 	mPreviewRunning = false;
 	mCamera.release();
 
-    Log.i(TAG, "surfaceDestroyed!");
+	Log.i(TAG, "surfaceDestroyed!");
   }
 
   /** Picture Callback */
   Camera.PictureCallback jpegCallback = new Camera.PictureCallback() {
-    public void onPictureTaken(byte[] imageData, Camera c) {
-      
-	  Uri imageFileUri =
-	    getContentResolver().insert(Media.EXTERNAL_CONTENT_URI, 
-		    new ContentValues());
-	  
+	public void onPictureTaken(byte[] imageData, Camera c) {
+
+	  Uri imageFileUri = getContentResolver().insert(
+		  Media.EXTERNAL_CONTENT_URI, new ContentValues());
+
 	  try {
-	    OutputStream imageFileOS = 
-	      getContentResolver().openOutputStream(imageFileUri);
-	    
-	    imageFileOS.write(imageData);
-	    imageFileOS.flush();
-	    imageFileOS.close();
-	    
-	  } catch (FileNotFoundException e) {
-	    Log.e(TAG, "File not found!");
+		OutputStream imageFileOS = getContentResolver().openOutputStream(
+			imageFileUri);
+
+		imageFileOS.write(imageData);
+		imageFileOS.flush();
+		imageFileOS.close();
+
 	  } catch (IOException e) {
-	    Log.e(TAG, "IOException!");
+		Log.e(TAG, "IOException!");
 	  }
-	    
+
 	  mLinearLayout.setVisibility(View.INVISIBLE);
-	  
-	  Toast.makeText(CameraActivity.this, imageFileUri.getPath(), 
+
+	  Toast.makeText(CameraActivity.this, imageFileUri.getPath(),
 		  Toast.LENGTH_SHORT).show();
-	  
-      Log.i(TAG, imageFileUri.getPath());
-      
-   }  
-  }; 
+
+	  Log.i(TAG, imageFileUri.getPath());
+
+	}
+  };
 }
